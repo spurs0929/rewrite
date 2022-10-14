e86eae1d2e28ae68a7903a60192cacbea80d32fa
spurs0929 <briankang0929@gmail.com>
2022-10-14 10:30:09 +0800

    add 重寫Object.is方法

---
+++ Object/is.js
@@ -0,0 +1,10 @@
+// Object.is重寫
+Object.$is = function(x, y){
+  if(a === b){
+    // 利用 1 / +0 = Infinity 和 1 / -0 = -Infinity來判斷 a 和 b 是 +0 與 -0 的情況
+    return a !== 0 || 1 / a === 1 / b;
+  }
+
+  // 判斷 a 和 b 為 NaN 的情況
+  return a !== a && b !== b;
+}
