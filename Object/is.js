610e6164a4f12ce67dcc30ae09a6f8c5e8e0cd24
spurs0929 <briankang0929@gmail.com>
2023-01-03 11:27:25 +0800

    debug 參數錯誤

--- Object/is.js
+++ Object/is.js
@@ -1,5 +1,5 @@
 // Object.is重寫
-Object.$is = function(x, y){
+Object.$is = function(a, b){
   if(a === b){
     // 利用 1 / +0 = Infinity 和 1 / -0 = -Infinity來判斷 a 和 b 是 +0 與 -0 的情況
     return a !== 0 || 1 / a === 1 / b;
@@ -8,3 +8,5 @@ Object.$is = function(x, y){
   // 判斷 a 和 b 為 NaN 的情況
   return a !== a && b !== b;
 }
+
+
